9978c1266f36d8d0af91f4157c99d93c10ac6f96
NarayanRGuttal <yan_guttal@yahoo.com>
2017-12-26 22:02:28 +0530

    FirstGitHub

---
+++ AuthenticationServer/App_Start/Startup.cs
@@ -0,0 +1,43 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using AuthenticationServer.Providers;
+
+[assembly: OwinStartup(typeof(AuthenticationServer.Startup))]
+
+namespace AuthenticationServer
+{
+
+	public  class Startup
+	{
+		public void Configuration(IAppBuilder appBuilder)
+		{
+			ConfigureAuth(appBuilder);
+		}
+
+		public void ConfigureAuth(IAppBuilder appBuilder)
+		{
+			OAuthAuthorizationServerOptions OAuthOptions = new OAuthAuthorizationServerOptions()
+			{
+				TokenEndpointPath = new PathString("/token"),
+				Provider = new OAuthTokenProvider(),
+				AccessTokenExpireTimeSpan = TimeSpan.FromDays(2),
+				AllowInsecureHttp = true
+
+			};
+
+
+			appBuilder.UseOAuthAuthorizationServer(OAuthOptions);
+			appBuilder.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+			//HttpConfiguration httpConfig = new HttpConfiguration();
+			//WebApiConfig.Register(httpConfig);
+
+		}
+
+	}
+}
---
+++ AuthenticationServer/Providers/OAuthTokenProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using System.Security.Claims;
+
+namespace AuthenticationServer.Providers
+{
+	public class OAuthTokenProvider : OAuthAuthorizationServerProvider 
+	{
+		public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+		{
+			context.Validated();
+		}
+
+		public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+		{
+			if (context.UserName != context.Password)
+			{
+				context.SetError("Invalid_grant", " The User name or Password is InCorrect");
+				return;
+			}
+
+			var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+			identity.AddClaim(new Claim("sub", context.UserName));
+			identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+
+			context.Validated(identity);
+		} 
+	}
+}
